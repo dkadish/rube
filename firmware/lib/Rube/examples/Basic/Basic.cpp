7112a3838b03b02e10f030d3de99386cd0abd077
David Kadish <david.kadish@gmail.com>
2018-02-13 14:26:58 +0100

    Updates to get IMU working properly

--- firmware/lib/Rube/examples/Basic/Basic.cpp
+++ firmware/lib/Rube/examples/Basic/Basic.cpp
@@ -1,21 +1,36 @@
-//
-// Created by David Kadish on 12/02/2018.
-//
+/***********************************************************************************************************************
+ * Created by David Kadish on 12/02/2018.
+ *
+ * TODO:
+ * [x] Winch
+ * [x] IMU
+ * [ ] SD Card Logging
+ * [ ] XBee Communication
+ * [ ] Add interval bump to winch code
+ */
 
 #include <Arduino.h>
 #include <logging.h>
 #include <pindefs.h>
-#include <Winch.h>
 
+// Winch
+#include <Winch.h>
 Winch winch(0, ENC1A, ENC1B,
         M1_BIN1, M1_BIN2, M1_PWMB, 1, M1_STBY,
         10.0, 5.0, 0.5);
 
+// IMU
+#include <NXPMotionSense.h>
+#include <i2c_t3.h>
+NXPMotionSense imu(&Wire1, &Wire3);
+NXPSensorFusion imu_filter;
 
 // Function to run the "loop" once
 void loopOnce();
 
 void setup(){
+    Wire1.begin(I2C_MASTER, 0x00, I2C_PINS_37_38, I2C_PULLUP_EXT, 400000); // Teensy
+    Wire3.begin(I2C_MASTER, 0x00, I2C_PINS_56_57, I2C_PULLUP_EXT, 400000); // Teensy
 
     // Serial connection
     Serial.begin(9600);
@@ -26,13 +41,25 @@ void setup(){
     winch.setup();
     Serial.printf("Finished setting up Winch (%i ms)\n", millis());
 
-    
+    // IMU
+    imu.begin();
+    imu_filter.begin(100);
+    Serial.printf("Finished setting up IMU (%i ms)\n", millis());
 
+    /* NOTE
+     * It seems like the first read never reads, so you need to do this to get it ready to send data.
+     */
+    while(!imu.available()) {
+        Serial.printf("Waiting for IMU...(%i ms)\n", millis());
+        delay(100);
+    }
 
     Serial.printf("Looping once (%i ms)\n", millis());
     loopOnce();
     Serial.printf("Finished loop (%i ms)\n", millis());
 
+    Serial.println();
+    Serial.printf("Testing Log Messages (%i ms)\n", millis());
     TRACE("This is a TRACE message.");
     DEBUG("This is a DEBUG message.");
     INFO("This is a INFO message.");
@@ -47,6 +74,33 @@ void loopOnce(){
     winch.loop();
     Serial.print("The current position of the winch is ");
     Serial.println(winch.current_position());
+
+    // IMU
+    float ax, ay, az;
+    float gx, gy, gz;
+    float mx, my, mz;
+    float roll, pitch, heading;
+
+    if (imu.available()) {
+        // Read the motion sensors
+        imu.readMotionSensor(ax, ay, az, gx, gy, gz, mx, my, mz);
+
+        // Update the SensorFusion filter
+        imu_filter.update(gx, gy, gz, ax, ay, az, mx, my, mz);
+
+        // print the heading, pitch and roll
+        roll = imu_filter.getRoll();
+        pitch = imu_filter.getPitch();
+        heading = imu_filter.getYaw();
+        Serial.print("Orientation: ");
+        Serial.print(heading);
+        Serial.print(" ");
+        Serial.print(pitch);
+        Serial.print(" ");
+        Serial.println(roll);
+    } else {
+        Serial.println("Oops, IMU not available. Something is wrong.");
+    }
 
 
 }
